19cd6dbd11dec2d5642377c1b3c3bc2a156f739a
Leandro Rafael Esteves Torralvo <ltorralvo365@gmail.com>
2026-05-30 15:00:25 +0100

    Test ReDish Page

---
+++ katalon_tests/Test Suites/testes_incremento1.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testes_incremento1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>134c5a54-069d-49b7-a221-77ba14dcbd0d</testSuiteGuid>
+   <testCaseLink>
+      <guid>d0e9f2d7-b888-47df-83a9-68b53da0078f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testes_incremento1/US1.1</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>77207dcb-f3d2-45c9-a1d0-4f878ca5fe20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testes_incremento1/US2.2</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>60ef0ad3-fc36-4572-ada2-0a755f6a459b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testes_incremento1/US2.3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
